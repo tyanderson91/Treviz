ceabb5241539a3d4994516a58d32a27625d3d365
Tyler Anderson <tyanderson91@gmail.com>
2019-02-26 00:06:26 -0800

    Initial Commit

---
+++ Treviz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Treviz
+//
+//  Created by Tyler Anderson on 2/26/19.
+//  Copyright © 2019 Tyler Anderson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Treviz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Treviz
+//
+//  Created by Tyler Anderson on 2/26/19.
+//  Copyright © 2019 Tyler Anderson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
