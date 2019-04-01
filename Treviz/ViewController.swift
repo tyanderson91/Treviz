d09b560c0eef06adf3189ccd3c51c060be340cee
Tyler Anderson <tyanderson91@gmail.com>
2019-03-31 23:40:15 -0700

    Added custom view controller that tracks representedObject through child controllers

---
+++ Treviz/ViewController.swift
@@ -0,0 +1,50 @@
+/*
+Abstract:
+NSViewController subclass controlling the user interface of the Document.
+This is a custom subclass that passes the Analysis document data to all child view controllers
+*/
+
+import Cocoa
+//TODO: turn this into some kind of extension on top of NSViewController
+class ViewController: NSViewController {
+
+    /// - Tag: setRepresentedObjectExample
+    override var representedObject: Any? {
+        didSet {
+            // Pass down the represented object to all of the child view controllers.
+            for child in children {
+                child.representedObject = representedObject
+            }
+        }
+    }
+
+    weak var analysis: Analysis? {
+        if let analysisRepresentedObject = representedObject as? Analysis {
+            return analysisRepresentedObject
+        }
+        return nil
+    }
+
+}
+
+
+class SplitViewController: NSSplitViewController {
+    
+    /// - Tag: setRepresentedObjectExample
+    override var representedObject: Any? {
+        didSet {
+            // Pass down the represented object to all of the child view controllers.
+            for child in children {
+                child.representedObject = representedObject
+            }
+        }
+    }
+    
+    weak var analysis: Analysis? {
+        if let analysisRepresentedObject = representedObject as? Analysis {
+            return analysisRepresentedObject
+        }
+        return nil
+    }
+    
+}
